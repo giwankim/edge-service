a73a5ee75dd21cc8b342ccdee4d88d22b3d88d43
Giwan Kim <giwan.kim@gmail.com>
2025-09-18 17:42:35 +0900

    ci(workflows): enhance build script and refactor test runner configuration
    - Updated `commit-stage.yml` to use `./gradlew build` with `-i` and `--stacktrace` flags for improved debugging.
    - Refactored `TestEdgeServiceApplication` for better readability with formatted chaining.

--- src/test/kotlin/com/polarbookshop/edgeservice/TestEdgeServiceApplication.kt
+++ src/test/kotlin/com/polarbookshop/edgeservice/TestEdgeServiceApplication.kt
@@ -4,5 +4,7 @@ import org.springframework.boot.fromApplication
 import org.springframework.boot.with
 
 fun main(args: Array<String>) {
-    fromApplication<EdgeServiceApplication>().with(TestcontainersConfiguration::class).run(*args)
+    fromApplication<EdgeServiceApplication>()
+        .with(TestcontainersConfiguration::class)
+        .run(*args)
 }
