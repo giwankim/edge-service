0c41838f95ddf85f47f564411fdb0d75f37a6bbc
Giwan Kim <giwan.kim@gmail.com>
2025-09-21 21:13:00 +0900

    feat(config): add rate limiter configuration and key resolver
    - Introduced `RateLimiterConfig` class with a `KeyResolver` bean for rate-limiting logic.
    - Updated comments in `application.yml` to standardize capitalization.

---
+++ src/main/kotlin/com/polarbookshop/edgeservice/config/RateLimiterConfig.kt
@@ -0,0 +1,15 @@
+package com.polarbookshop.edgeservice.config
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import reactor.core.publisher.Mono
+
+@Configuration
+class RateLimiterConfig {
+    @Bean
+    fun keyResolver(): KeyResolver =
+        KeyResolver { _ ->
+            Mono.just("anonymous")
+        }
+}
