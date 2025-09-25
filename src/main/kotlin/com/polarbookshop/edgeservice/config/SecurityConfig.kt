b79a75393355c794a7b0af8806991d78614c6b64
Giwan Kim <giwan.kim@gmail.com>
2025-09-25 17:21:37 +0900

    feat(config): enhance security with custom authentication entry point and public route configuration
    - Added `HttpStatusServerEntryPoint` to handle unauthorized access with `401 UNAUTHORIZED` response.
    - Configured public routes for static assets and the root path to allow unrestricted access.
    - Updated `SecurityConfig` to improve route authorization and exception handling setup.

--- src/main/kotlin/com/polarbookshop/edgeservice/config/SecurityConfig.kt
+++ src/main/kotlin/com/polarbookshop/edgeservice/config/SecurityConfig.kt
@@ -2,13 +2,16 @@ package com.polarbookshop.edgeservice.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.config.web.server.invoke
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.pathMatchers
 
 @Configuration(proxyBeanMethods = false)
 @EnableWebFluxSecurity
@@ -20,7 +23,11 @@ class SecurityConfig {
     ): SecurityWebFilterChain =
         http {
             authorizeExchange {
+                authorize(pathMatchers("/", "/*.css", "/*.js", "/favicon.ico"), permitAll)
                 authorize(anyExchange, authenticated)
+            }
+            exceptionHandling {
+                authenticationEntryPoint = HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)
             }
             oauth2Login { }
             logout {
@@ -29,7 +36,8 @@ class SecurityConfig {
         }
 
     private fun oidcLogoutSuccessHandler(clientRegistrationRepository: ReactiveClientRegistrationRepository): ServerLogoutSuccessHandler {
-        val oidcLogoutSuccessHandler = OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository)
+        val oidcLogoutSuccessHandler =
+            OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository)
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}")
         return oidcLogoutSuccessHandler
     }
