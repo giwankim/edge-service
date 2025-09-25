947da236b4e26e8702106328ae01a7f73d845729
Giwan Kim <giwan.kim@gmail.com>
2025-09-25 13:18:12 +0900

    feat(config): add OIDC logout success handler to `SecurityConfig`
    - Integrated `OidcClientInitiatedServerLogoutSuccessHandler` for handling logout with post-logout redirection.
    - Updated `SecurityConfig` to wire `ReactiveClientRegistrationRepository` for OIDC logout handling.

--- src/main/kotlin/com/polarbookshop/edgeservice/config/SecurityConfig.kt
+++ src/main/kotlin/com/polarbookshop/edgeservice/config/SecurityConfig.kt
@@ -5,17 +5,32 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.config.web.server.invoke
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler
 
 @Configuration(proxyBeanMethods = false)
 @EnableWebFluxSecurity
 class SecurityConfig {
     @Bean
-    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
+    fun springSecurityFilterChain(
+        http: ServerHttpSecurity,
+        clientRegistrationRepository: ReactiveClientRegistrationRepository,
+    ): SecurityWebFilterChain =
         http {
             authorizeExchange {
                 authorize(anyExchange, authenticated)
             }
             oauth2Login { }
+            logout {
+                logoutSuccessHandler = oidcLogoutSuccessHandler(clientRegistrationRepository)
+            }
         }
+
+    private fun oidcLogoutSuccessHandler(clientRegistrationRepository: ReactiveClientRegistrationRepository): ServerLogoutSuccessHandler {
+        val oidcLogoutSuccessHandler = OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository)
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}")
+        return oidcLogoutSuccessHandler
+    }
 }
